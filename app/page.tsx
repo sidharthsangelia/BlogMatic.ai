cb2d2962c196d9870c49fa266e9e75032d366b95
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-07-03 01:19:16 +0530

    add banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import Banner from "@/components/ui/Home/Banner";
  
 
 export default function Home() {
   return (
-   <main>
-    <Button/>
+   <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px), transparent_1px]  [background-size:20px_20px]">
+   <Banner/>
+   {/* <Divider/>
+   <HowItWorks/>
+   <Divider/>
+   <Pricing/>
+   <Divider/>
+   <Footer/> */}
    </main>
   );
 }
---
+++ components/ui/Home/Banner.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { Button } from "../button";
+import { ArrowRight } from "lucide-react";
+
+export default function Banner() {
+  return (
+    <section className="lg:max-w-6xl mx-auto flex flex-col items-center justify-center py-28 sm:pt-32 transition-all animate-in">
+      <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-center px-4 lg:max-w-4xl mb-6">
+        Turn your words into{" "}
+        <span className="underline underline-offset-0 decoration-dashed decoration-purple-200">
+          captivating
+        </span>{" "}
+        blog posts
+      </h1>
+      <h2 className="text-center  sm:text-xl px-4 lg:max-w-4xl">
+        Convert your videos into engaging content with our AI-powered blog
+        writing tool.
+      </h2>
+      <Button
+        variant={"link"}
+        className=" mt-6 flex gap-2 item-center text-xl rounded-full px-12 py-8 lg:mt-20 bg-gradient-to-r from-purple-500 to-purple-600 text-white hover:from-purple-600 hover:to-purple-700 transition-colors font-bold hover:no-underline"
+      >
+        <Link href="/pricing">Get BlogMatic AI <ArrowRight className=""/></Link>
+      </Button>
+    </section>
+  );
+}
