5790d68f990634844cb09cf00edc5b13a59d9be9
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-07-03 12:11:43 +0530

    created custom sign-in sign-up routes added basic auth ui of clerk

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ import Banner from "@/components/Home/Banner";
 
 export default function Home() {
   return (
-   <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px), transparent_1px]  [background-size:20px_20px]">
-   <Banner/>
-   {/* <Divider/>
+    <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px), transparent_1px]  [background-size:20px_20px]">
+      
+      <Banner />
+      {/* <Divider/>
    <HowItWorks/>
    <Divider/>
    <Pricing/>
    <Divider/>
    <Footer/> */}
-   </main>
+    </main>
   );
 }
--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -2,6 +2,14 @@ import { Ghost } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
 const NavLink = ({
   href,
   children,
@@ -39,7 +47,18 @@ export default function Header() {
       </div>
       <div className="flex lg:justify-end lg:flex-1">
         <NavLink href="/dashboard">Upload a Video</NavLink>
-        {/* user profile  */}
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+          
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
       <NavLink href="/sign-in">Sign In</NavLink>
     </nav>
